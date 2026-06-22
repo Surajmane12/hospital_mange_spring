f8316abd1a3986b0fa38e6e4e536ba3d351583ee
surajm-kinetic-data <Suraj.mane@kinetic-data.com>
2026-06-22 16:51:15 +0530

    Hibernate Learning!!

--- src/main/java/com/example/hospitalmanage/entity/Patient.java
+++ src/main/java/com/example/hospitalmanage/entity/Patient.java
@@ -1,15 +1,21 @@
 package com.example.hospitalmanage.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Entity
 @ToString
+@Table(
+         uniqueConstraints = {
+                 @UniqueConstraint(name = "unique_Patient_Emails", columnNames = {"email"}),
+                 @UniqueConstraint(name = "unique_patient_name_email", columnNames = {"name","dob"})
+         },
+        indexes = {
+                 @Index(name="idx_patient_birth_date", columnList = "dob")
+        }
+)
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
